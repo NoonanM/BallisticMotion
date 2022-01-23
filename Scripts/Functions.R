cb9a26d71220ebfe154e63bb39f20711f39b6d11
Michael Noonan <michaelnoonan@Michaels-iMac.local>
2022-01-22 17:57:38 -0800

    Initial commit

---
+++ Scripts/Functions.R
@@ -0,0 +1,254 @@
+# This scripts generates the functions necessary for carrying out the 
+# simulation study aimed at exploring the evolution of ballistic motion
+
+
+#Written by Michael Noonan
+
+#Last updated: Jan 22nd 2022
+
+
+#----------------------------------------------------------------------
+# Package import
+
+library(ctmm)
+
+#----------------------------------------------------------------------
+# Calculate the euclidean distance between two points
+#----------------------------------------------------------------------
+
+SLD <- function(x_1, y_1, x_2, y_2){
+  sqrt((x_1 - x_2)^2 + (y_1 - y_2)^2)
+}
+
+#----------------------------------------------------------------------
+# Generate prey var[position] based on mass (in g)
+#----------------------------------------------------------------------
+
+# Model comes from Noonan et al. 2020  https://doi.org/10.1111/cobi.13495
+
+prey.SIG <- function(mass, variance = FALSE) {
+  #Calculate
+  HR <- 0.5078955 + 1.372162*log10(mass)
+  #Back transform
+  HR <- 10^(HR)
+  #Convert from 95% HR to var[position]
+  SIG <- HR/(-2*log(0.05)*pi)
+  #Add variance if desired
+  if(variance == TRUE){SIG <- rchisq(n = length(mass), df = SIG)}
+  #Return
+  return(SIG)
+}
+
+
+#----------------------------------------------------------------------
+# Generate prey E[tau_p] based on mass (in g)
+#----------------------------------------------------------------------
+
+# Model comes from Noonan et al. 2020  https://doi.org/10.1111/cobi.13495
+
+prey.tau_p <- function(mass, variance = FALSE) {
+  #Calculate
+  tau_p <- 1.2994292 + 0.8129125*log10(mass)
+  #Back transform
+  tau_p <- 10^(tau_p)
+  #Add variance if desired
+  if(variance == TRUE){tau_p <- rchisq(n = length(mass), df = tau_p)}
+  #Return
+  return(tau_p)
+}
+
+#----------------------------------------------------------------------
+# Generate prey E[tau_v] based on mass (in g)
+#----------------------------------------------------------------------
+
+# Model comes from Noonan et al. 2020  https://doi.org/10.1111/cobi.13495
+
+prey.tau_v <- function(mass, variance = FALSE) {
+  #Calculate
+  tau_v <- -1.365200 + 0.787177*log10(mass)
+  #Back transform
+  tau_v <- 10^(tau_v)
+  #Add variance if desired
+  if(variance == TRUE){tau_v <- rchisq(n = length(mass), df = tau_v)}
+  #Return
+  return(tau_v)
+}
+
+
+
+
+#----------------------------------------------------------------------
+# Generate predator var[position] based on mass (in g)
+#----------------------------------------------------------------------
+
+# Model comes from Noonan et al. 2020  https://doi.org/10.1111/cobi.13495
+
+pred.SIG <- function(mass, variance = FALSE) {
+  #Calculate
+  HR <- 1.089972 + 1.478050*log10(mass)
+  #Back transform
+  HR <- 10^(HR)
+  #Convert from 95% HR to var[position]
+  SIG <- HR/(-2*log(0.05)*pi)
+  #Add variance if desired
+  if(variance == TRUE){SIG <- rchisq(n = length(mass), df = SIG)}
+  #Return
+  return(SIG)
+}
+
+
+#----------------------------------------------------------------------
+# Generate predator E[tau_p] based on mass (in g)
+#----------------------------------------------------------------------
+
+# Model comes from Noonan et al. 2020  https://doi.org/10.1111/cobi.13495
+
+pred.tau_p <- function(mass, variance = FALSE) {
+  #Calculate
+  tau_p <- 1.612761 + 0.766461*log10(mass)
+  #Back transform
+  tau_p <- 10^(tau_p)
+  #Add variance if desired
+  if(variance == TRUE){tau_p <- rchisq(n = length(mass), df = tau_p)}
+  #Return
+  return(tau_p)
+}
+
+#----------------------------------------------------------------------
+# Generate predator E[tau_v] based on mass (in g)
+#----------------------------------------------------------------------
+
+# Model comes from Noonan et al. 2020  https://doi.org/10.1111/cobi.13495
+
+pred.tau_v <- function(mass, variance = FALSE) {
+  #Calculate
+  tau_v <- -0.1005302 + 0.7403169*log10(mass)
+  #Back transform
+  tau_v <- 10^(tau_v)
+  #Add variance if desired
+  if(variance == TRUE){tau_v <- rchisq(n = length(mass), df = tau_v)}
+  #Return
+  return(tau_v)
+}
+#----------------------------------------------------------------------
+# Generate E[mass_prey] based on mass_pred (in g)
+#----------------------------------------------------------------------
+
+# Model comes from Tucker & Rogers 2014  https://doi.org/10.1371/journal.pone.0106402
+
+prey.mass <- function(mass, variance = FALSE) {
+  #Convert to kg
+  mass <- mass * (1 %#% "g")
+  #Calculate
+  prey_mass <- -0.87 + 1.26*log10(mass)
+  #Back transform
+  prey_mass <- 10^(prey_mass)
+  #Convert to g
+  prey_mass <-prey_mass / (1 %#% "g")
+  #Add variance if desired
+  if(variance == TRUE){tau_v <- rchisq(n = length(mass), df = prey_mass)}
+  #Return
+  return(prey_mass)
+}
+
+#----------------------------------------------------------------------
+# Generate raster of food patches based on mass_prey (g)
+#----------------------------------------------------------------------
+
+patches <- function(mass, n = 20) {
+  
+  # var[position]
+  SIG <- prey.SIG(mass_prey)
+  
+  # Range of raster based on 99.9% HR area
+  EXT <- round(sqrt((-2*log(0.001)*pi)* SIG))
+  
+  #patch width
+  N <- EXT/n
+  
+  #Build the raster
+  FOOD <- raster(matrix(1,N,N),
+                 xmx=EXT, xmn=-EXT,
+                 ymx=EXT, ymn=-EXT)
+  
+  #Return the raster of food patches
+  return(FOOD)
+}
+
+#----------------------------------------------------------------------
+# Count the number of patches visited (assumes immediate renewal)
+#----------------------------------------------------------------------
+
+grazing <- function(track, habitat, metric = "patches") {
+   
+   #Patch identities
+   IDs <- suppressWarnings(cellFromXY(habitat,
+                                      SpatialPoints.telemetry(track))
+                           )
+   # Count the number of times it moved to a new food patch
+   PATCHES <- sum(diff(IDs) != 0)
+   
+   #Mean time between patches 
+   TIME <- mean(rle(c(FALSE, diff(IDs) != 0))$lengths)
+
+   if(metric == "patches"){return(PATCHES)}
+   if(metric == "time"){return(TIME)}
+ }
+
+#----------------------------------------------------------------------
+# Determine "Lifespan" and sampling interval based on mass_prey (g)
+#----------------------------------------------------------------------
+
+sampling <- function(mass, crossings = 50) {
+  
+  # total lifespan (based on number of range crossings)
+  lifespan <- round(prey.tau_p(mass)*crossings)
+  
+  # sampling interval (tau_v)
+  interval <- round(prey.tau_v(mass))
+  
+  #"Lifespan" and sampling interval for the simulations
+  t <- seq(0,
+           lifespan,
+           interval)
+  
+  #return the vector of sampling times
+  return(t)
+}
+
+
+#----------------------------------------------------------------------
+# Prey fitness function
+#----------------------------------------------------------------------
+
+prey.fitness <- function(benefits, mass, costs = NULL, crossings = 50, calories = 10){
+  
+  # Basal metabolic rate (in kj/day) from Nagy 1987 https://doi.org/10.2307/1942620 
+  BMR <- 0.774 + 0.727*log10(mass)
+  
+  #Back transform 
+  BMR <- 10^BMR
+  
+  # total lifespan in days (based on number of range crossings)
+  lifespan <- round(prey.tau_p(mass)*crossings) /60/60/24
+  
+  #Total energetic cost
+  COST <- BMR * lifespan
+  
+  # Excess energy
+  excess <- benefits*calories - COST
+  
+  #standardise the excess energy values
+  excess_stand <- (excess - mean(excess))/sd(excess)
+  
+  #Define number of prey offspring based on their excess energy
+  # Individuals that had the shortest time between patches have more offspring
+  offspring <- round(excess_stand) 
+  offspring <- ctmm:::clamp(offspring, min = 0, max = Inf) #Clamp the minimum to 0
+  
+  offspring
+}
+
+#----------------------------------------------------------------------
+# 
+#----------------------------------------------------------------------
---
+++ Scripts/Sims_Prey_B.R
@@ -0,0 +1,258 @@
+#Script for studying the evolution of ballistic length scales
+# in pred/prey systems
+
+#Written by Michael Noonan
+
+#Last updated: Jan 22nd 2022
+
+#----------------------------------------------------------------------
+# Preamble
+#----------------------------------------------------------------------
+
+#Set the working directory
+setwd("~/Dropbox (Personal)/UBC/Projects/BallisticMotion")
+
+#Set the random seed
+set.seed(1)
+
+#Import necessary packages
+library(ctmm)
+library(raster)
+library(extraDistr)
+
+#Source the functions
+source("Scripts/Functions.R")
+
+
+#----------------------------------------------------------------------
+# Set up the global parameters for the simulation
+#----------------------------------------------------------------------
+
+#Predator mass (g)
+mass_pred <- 15000
+
+#Prey mass (g)
+mass_prey <- prey.mass(mass_pred)
+
+#"Lifespan" and sampling interval for the simulations
+t <- sampling(mass_prey)
+
+#Number of preds & prey in each "arena"
+n_prey <- 10
+n_pred <- 1
+
+#Number of "arenas"
+REPS <- 50
+
+#Number of generations
+GENS <- 1000
+
+# Build the raster of food patches for prey to feed on
+FOOD <- patches(mass_pred, n = 30)
+
+
+#Lists for storing results and drawing params
+prey_res <- list()
+prey_details <- list()
+
+#----------------------------------------------------------------------
+# Run the simulation
+#----------------------------------------------------------------------
+
+#Loop over the number of generations
+for(G in 1:GENS){
+  
+  #Empty lists for storing the results of the current generation
+  prey <- list()
+  
+  for(R in 1:REPS){
+    
+    #Generate the prey movement models
+    #If the first gen, generate movement parameters from the mass functions
+    if(G == 1){
+      
+      PREY_mods <- list()
+      for(i in 1:n_prey){
+        # Prey movement parameters
+        prey_tau_p <- prey.tau_p(mass_prey)
+        prey_tau_v <- prey.tau_v(mass_prey, variance = TRUE)
+        prey_sig <- prey.SIG(mass_prey)
+        prey_lv <- sqrt((prey_tau_v/prey_tau_p)*prey_sig)
+        
+        PREY_mods[[i]] <- ctmm(tau = c(prey_tau_p,prey_tau_v),
+                               mu = c(0,0),
+                               sigma = prey_sig)
+      } #Closes loop over n_prey
+    } #Closes if gen 1 ask 
+    
+    
+    #If any other generation, draw movement from pool of offspring params
+    if(G != 1){
+      
+      PREY_mods <- list()
+      for(i in 1:n_prey){
+        # Prey movement parameters
+        prey_tau_p <- sample(PREY_tau_p, 1)
+        prey_tau_v <- sample(PREY_tau_v, 1) + rnorm(1, 0, 2) 
+        prey_tau_v <- ctmm:::clamp(prey_tau_v, min = 0, max = Inf) #Clamp the minimum to 0
+        prey_sig <- sample(PREY_sig, 1)
+        prey_lv <- sqrt((prey_tau_v/prey_tau_p)*prey_sig)
+        
+        PREY_mods[[i]] <- ctmm(tau = c(prey_tau_p,prey_tau_v),
+                               mu = c(0,0),
+                               sigma = prey_sig)
+      } #Closes loop over n_prey
+    } #Closes if not gen 1 ask 
+    
+    
+    #Simulate the prey movement
+    PREY_tracks <- list()
+    for(i in 1:n_prey){
+      PREY_tracks[[i]] <- simulate(PREY_mods[[i]], t = t)
+    }
+    
+    # Calculate prey benefits
+    benefits_prey <- vector()
+    for(i in 1:n_prey){
+      benefits_prey[i] <- grazing(PREY_tracks[[i]], FOOD)
+      
+    }
+    
+    #Calculate prey fitness
+    offspring_prey <- prey.fitness(benefits_prey, mass_prey)
+    
+    
+    #Get the values of the prey movement model parameters
+    prey_lvs <- vector()
+    prey_TAU_V <- vector() 
+    prey_TAU_P <- vector()
+    prey_SIGMA <- vector()
+    for(i in 1:n_prey){
+      prey_TAU_V[i] <- PREY_mods[[i]]$tau["velocity"]
+      prey_TAU_P[i] <- PREY_mods[[i]]$tau["position"]
+      prey_SIGMA[i] <- ctmm:::area.covm(PREY_mods[[i]]$sigma)
+      prey_lvs[i] <- sqrt((prey_TAU_V[i]/prey_TAU_P[i])*prey_SIGMA[i])
+    }
+    
+    #Summarise the results of the prey
+    prey[[R]] <- data.frame(generation = G,
+                            tau_p = prey_TAU_P,
+                            tau_v = prey_TAU_V,
+                            sig = prey_SIGMA,
+                            lv = prey_lvs,
+                            patches = benefits_prey,
+                            offspring = offspring_prey)
+  }
+  
+  #Compile the results from the generation
+  prey <- do.call(rbind, prey)
+  
+  # Save the results
+  # prey
+  prey_res[[G]] <- data.frame(generation = G,
+                              lv = mean(prey$lv),
+                              var = var(prey$lv))
+  
+  prey_details[[G]] <- prey
+  
+  
+  #Set up the parameters for the next generation based on
+  #Fitness of current generation
+  PREY_tau_p <- vector()
+  PREY_tau_v <- vector()
+  PREY_sig <- vector()
+  for(i in 1:nrow(prey)){
+    if(prey[i,"offspring"] >0){
+      PREY_tau_p <- c(prey_tau_p,
+                      rep(prey[i,"tau_p"], prey[i,"offspring"]))
+      
+      PREY_tau_v <- c(prey_tau_v,
+                      rep(prey[i,"tau_v"], prey[i,"offspring"]))
+      
+      PREY_sig <- c(prey_sig,
+                    rep(prey[i,"sig"], prey[i,"offspring"]))
+      
+    } #Closes the if statement
+  } #Closes the loop over the number of prey
+  
+  print(G)
+  save(prey_res, file = 'Results/lv_Evo_Prey_Ben.Rda')
+  save(prey_details, file = 'Results/lv_Evo_Prey_Ben_details.Rda')
+}
+
+#Compile the results
+load('Results/lv_Evo_Prey_Ben.Rda')
+load('Results/lv_Evo_Prey_Ben_details.Rda')
+
+prey_res <- do.call(rbind, prey_res)
+prey_details <- do.call(rbind, prey_details)
+
+#Compare with starting values
+PREY_LV <- prey_res$lv[1] #(sqrt((tau_v/tau_p)*sig))
+
+#par(mfrow = c(1,2))
+plot(lv/PREY_LV ~ generation,
+     data = prey_res,
+     type = "l",
+     col = "red",
+     ylim = c(0,2),
+     ylab = "Relative change in lv",
+     xlab = "Generation")
+abline(h = 1, col = 'grey30', lty = 'dashed')
+
+# lines((lv + var)/LV ~ generation,
+#       data = prey_res,
+#       col = 'grey70')
+# lines((lv - var)/LV ~ generation,
+#       data = prey_res,
+#       col = 'grey70')
+polygon(c(prey_res$generation, rev(prey_res$generation)), c((prey_res$lv - prey_res$var)/PREY_LV, rev((prey_res$lv + prey_res$var)/PREY_LV)),
+        col = adjustcolor("grey70",alpha.f=0.5), border=NA)
+lines(lv/PREY_LV ~ generation,
+      data = prey_res,
+      col = "red")
+
+
+plot(offspring ~ lv,
+     data = prey_details,
+     #type = "l",
+     col = "red",
+     #ylim = c(0,7),
+     xlab = "Ballistic lengthscale (m)",
+     ylab = "Offspring")
+abline(lm(offspring ~ lv,
+          data = prey_details), lty = 'dashed')
+
+
+
+
+plot(patches ~ generation,
+     data = prey_details,
+     #type = "l",
+     col = "red",
+     #ylim = c(0,7),
+     xlab = "Generation",
+     ylab = "Patches")
+abline(lm(patches ~ generation,
+          data = prey_details), lty = 'dashed')
+
+
+plot(lv ~ generation,
+     data = prey_details,
+     #type = "l",
+     col = "red",
+     #ylim = c(0,7),
+     xlab = "Generation",
+     ylab = "Ballistic lengthscale (m)")
+abline(lm(lv ~ generation,
+          data = prey_details), lty = 'dashed')
+
+plot(tau_v ~ generation,
+     data = prey_details,
+     #type = "l",
+     col = "red",
+     #ylim = c(0,7),
+     xlab = "Generation",
+     ylab = "Directional persistance (sec)")
+abline(lm(tau_v ~ generation,
+          data = prey_details), lty = 'dashed')
